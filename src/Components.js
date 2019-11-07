33295298a62578350396f6fb1723ec49dd3e66f1
rafael <6116829+rafaelnco@users.noreply.github.com>
2019-11-07 16:45:23 -0300

    Optimize components

--- src/Components.js
+++ src/Components.js
@@ -4,65 +4,36 @@ import PropTypes from "prop-types";
 
 import { applyVariations } from "./Appearance";
 
-const Section = ({
-  _DefaultTagType = "div",
-  TagType = _DefaultTagType,
-  hoverIn,
-  hoverOut,
-  pressIn,
-  pressOut,
-  style,
-  ...props
-}) => {
-  const [pressed, setPressed] = useState(false);
-  const handlers = {
-    onMouseEnter: () => {
-      if (hoverIn) hoverIn();
-    },
-    onMouseDown: () => {
-      if (pressIn) pressIn();
-      setPressed(true);
-    },
-    onMouseUp: () => {
-      if (pressed && pressOut) pressOut();
-      setPressed(false);
-    },
-    onMouseLeave: () => {
-      if (pressed) {
-        if (pressOut) pressOut();
-        setPressed(false);
-      }
-      if (hoverOut) hoverOut();
-    }
-  };
-  return (
-    <TagType
-      {...handlers}
-      {...props}
-      style={Object.assign(
-        TagType === _DefaultTagType && { display: "flex" },
-        applyVariations(props),
-        style
-      )}
-    />
-  );
-};
+const Bare = ({ Tag = "div", ...props }) => <Tag {...applyVariations(props)} />;
+
+const Section = props => <Bare use-mouse flex {...props} />;
+
+const Text = props => <Bare Tag="p" primary-foreground {...props} />;
+
+Text.Span = props => <Bare Tag="span" primary-foreground {...props} />;
+
+const Link = props => <Text Tag="a" no-decoration link-foreground {...props} />;
+
+const Image = props => <Bare Tag="img" normal-round normal-shadow {...props} />;
+
+const Path = props => <Bare Tag="path" primary-fill {...props} />;
+
+const Input = props => (
+  <Bare Tag="input" transparent-background full-width no-border {...props} />
+);
 
 const Navigator = ({ scene, children, controller }) => {
   return (
-    <Section full style={{ display: "block", position: "relative" }}>
+    <Section full block relative>
       {children.map((child, index) => (
         <Section
-          style={{
-            opacity: `${~~(scene === index)}`,
-            zIndex: ~~(scene === index),
-            transition: `opacity 600ms`,
-            position: "absolute",
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0
-          }}
+          absolute
+          zero-inset
+          giant-opacity-transition
+          full-opacity={scene === index}
+          one-apex={scene === index}
+          zero-opacity={scene !== index}
+          zero-apex={scene !== index}
         >
           <child.component {...controller} />
         </Section>
@@ -70,31 +41,5 @@ const Navigator = ({ scene, children, controller }) => {
     </Section>
   );
 };
-
-const Input = ({ children, style, ...props }) => (
-  <Section
-    TagType="input"
-    placeholder={children}
-    transparent-background
-    {...props}
-    style={{
-      width: "100%",
-      border: "none",
-      ...style
-    }}
-  />
-);
-
-const Text = props => <Section TagType="p" primary-foreground {...props} />;
-
-const Link = props => (
-  <Text TagType="a" no-decoration link-foreground {...props} />
-);
-
-const Image = props => (
-  <Section TagType="img" normal-round normal-shadow {...props} />
-);
-
-const Path = props => <Section TagType="path" primary-fill {...props} />;
 
 export { Section, Text, Link, Path, Input, Image, Navigator };
